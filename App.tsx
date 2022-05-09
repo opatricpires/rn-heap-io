8ee55db9dabe3d5c7e16f938b0833254028f43f5
opatricpires <patric.pires@outlook.com>
2022-05-09 16:22:43 -0300

    refactor(app): use heap navigation container

--- App.tsx
+++ App.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {Home} from './src/pages/Home';
-import {Details} from './src/pages/Deatils';
+import {Details} from './src/pages/Details';
+import Heap from '@heap/react-native-heap';
 
 const Stack = createNativeStackNavigator();
 
+const HeapNavigationContainer =
+  Heap.withReactNavigationAutotrack(NavigationContainer);
+
 const App = () => {
   return (
-    <NavigationContainer>
+    <HeapNavigationContainer>
       <Stack.Navigator
         initialRouteName="Home"
         screenOptions={{
@@ -17,7 +21,7 @@ const App = () => {
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Details" component={Details} />
       </Stack.Navigator>
-    </NavigationContainer>
+    </HeapNavigationContainer>
   );
 };
 
